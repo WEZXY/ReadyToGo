af560e2811fdcc5701a0d9de402bb604e0d8e462
Mohamed-Elmoslimany <elmoslimanym@gmail.com>
2026-08-31 05:07:04 +0300

    initial commit

---
+++ ReadyToGo.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import Qt
+
+class ReadyToGo(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+app = QApplication(sys.argv)
+window = ReadyToGo()
+window.show()
+sys.exit(app.exec_())
